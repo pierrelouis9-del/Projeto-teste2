379979991630af101d8e10d454378482566c61da
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-09-04 15:30:41 -0300

    V1.0.5

--- src/administrador/cadastraturma.jsx
+++ src/administrador/cadastraturma.jsx
@@ -1,2 +1,20 @@
+import { useState } from "react";
 
 
+function Cadastroturma() {
+    const [nome, setNome] = useState('');
+
+    function Escolha_professor() {
+
+    }
+
+    return (
+        <main className="container">
+            <input type="text"
+                aria-label="Escreve o nome da turma "
+                value={nome}
+                onChange={(e) => setNome(e.target.value)} />
+
+        </main>
+    );
+}
